d191b6ea17798b7505481f4a5d2e8c2a6aec59cb
kapil <kapil.personal9711@gmail.com>
2024-10-23 22:05:04 +0530

    added security middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
 dotenv.config();
-
 const app = express();
+
+// Setup security headers
+app.use(helmet());
+// body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Santize data
+app.use(mongoSanitize()); //nosql injection atack
+// prevent Parameter pollution
+app.use(hpp());
+
+module.find({ username, password });
 
 const PORT = process.env.PORT || 8000;
 const MODE = process.env.NODE_ENV || "production";
