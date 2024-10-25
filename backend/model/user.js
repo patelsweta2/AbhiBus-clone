f17e52f28aebe975fee6723f0ee7d55ed900a5dc
kapil <kapil.personal9711@gmail.com>
2024-10-25 19:45:15 +0530

    corrected user model and added methods on scehema

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,41 +1,55 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const phoneRegex = /^\d{10}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const options = {
   firstName: {
     type: String,
     required: [true, "First Name is required"],
+    minLength: [4, "FirstName Should have 4 characters"],
   },
   lastName: {
     type: String,
     required: [true, "Last Name is required"],
+    minLength: [4, "LastName Should have 4 characters"],
   },
   email: {
     type: String,
     required: [true, "Email is required"],
     unique: true,
+    validate: {
+      validator: (v) => emailRegex.test(),
+      message: (props) => `${props.value} is not a valid email`,
+    },
   },
   password: {
     type: String,
     required: [true, "Password is required"],
+    minLength: [8, "Password should have 8 characters"],
+    maxLength: [24, "Password should be less than 24 character"],
   },
   phoneNumber: {
-    type: String,
+    type: Number,
     required: true,
     unique: true,
     validate: {
-      validator: function (v) {
-        return /^\d{10}$/.test(v);
-      },
-      message: "Phone number must be 10 digits",
+      validator: (v) => phoneRegex.test(),
+      message: (props) => `Phone number must be 10 digits,(${props.value})`,
     },
   },
   gender: {
     type: String,
-    enum: ["male", "female", "others"],
     required: [true, "gender is required"],
+    enum: {
+      value: ["male", "female", "others"],
+      message: "{value} as gender not supported",
+    },
   },
   age: {
-    type: String,
+    type: Number,
     required: [true, "age is required"],
     min: 1,
     max: 100,
@@ -44,21 +58,45 @@ const options = {
     type: Boolean,
     default: false,
   },
-  verificationToken: {
-    type: String,
-    default: null,
+  isAdmin: {
+    type: Boolean,
+    default: false,
   },
-  resetPassword: {
-    type: String,
-    default: null,
-  },
-  resetPasswordExpires: {
-    type: Date,
-    default: null,
-  },
+  resetPasswordToken: String,
+  resetPasswordExpires: String,
 };
 
 const userSchema = new mongoose.Schema(options);
+
+//virtuals
+userSchema.virtual("fullName").get(function () {
+  return `${this.fullName} ${this.lastName}`;
+});
+
+// pre functions
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//methods
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//jason web tokens
+userSchema.methods.getAccessJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_ACCESS_SECRET_KEY, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_TIME,
+  });
+};
+
+userSchema.methods.getRefreshJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_SECRET_KEY, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_TIME,
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
